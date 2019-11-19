7add243f51eeb161a18129e504991199a604ab82
Ethan Zheng <y.zheng@wustl.edu>
2019-11-19 11:41:36 -0600

    Initial commit

---
+++ src/project/Snake.java
@@ -0,0 +1,13 @@
+package project;
+
+import edu.princeton.cs.introcs.StdDraw;
+
+public class Snake {
+	
+	//TODO: Implement the game of snake
+	public static void main(String[] args) {
+		
+		System.out.println("I'm a snaaaaaaaake! HISSSSSSSSSSSssssss.....");
+	}
+	
+}
